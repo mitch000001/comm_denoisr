a8c7280281807f30af016baf7763e6f508f0c969
Michael Wagner <mitch.wagna@gmail.com>
2014-04-12 23:13:10 +0200

    Add encryption

--- crypto/encrypter.go
+++ crypto/encrypter.go
@@ -1,29 +1,53 @@
 package crypto
 
 import (
-	_ "code.google.com/p/go.crypto/openpgp"
+	"bytes"
+	"code.google.com/p/go.crypto/openpgp"
 	_ "code.google.com/p/go.crypto/openpgp/armor"
 	_ "code.google.com/p/go.crypto/openpgp/errors"
 	"io"
+	"io/ioutil"
 )
 
 type Encrypter interface {
-	Encrypt(io.Reader) (string, error)
+	Encrypt(message io.Reader, password []byte) (string, error)
+	EncryptFor(message io.Reader, to []Receipient) (string, error)
 }
 
-type EncryptedMessage string
-
-func (e *EncryptedMessage) Write(p []byte) (n int, err error) {
-	return
-}
-
-func (e *EncryptedMessage) Read(p []byte) (n int, err error) {
-	return
-}
+type Receipient interface{}
 
 type OpenPgPEncrypter struct {
+	pubKeyRing openpgp.KeyRing
 }
 
-func (e *OpenPgPEncrypter) Encrypt(reader io.Reader) (encryptedMessage string, err error) {
+func (e *OpenPgPEncrypter) Encrypt(reader io.Reader, password []byte) (encryptedMessage string, err error) {
+	message, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return encryptedMessage, err
+	}
+	cipherBuffer := new(bytes.Buffer)
+	writeCloser, err := openpgp.SymmetricallyEncrypt(cipherBuffer, password, nil, nil)
+	if err != nil {
+		return encryptedMessage, err
+	}
+	writeCloser.Write(message)
+	writeCloser.Close()
+	encryptedMessage = string(cipherBuffer.Bytes())
+	return
+}
+
+func (e *OpenPgPEncrypter) EncryptFor(reader io.Reader, to []*openpgp.Entity) (encryptedMessage string, err error) {
+	message, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return encryptedMessage, err
+	}
+	cipherBuffer := new(bytes.Buffer)
+	writeCloser, err := openpgp.Encrypt(cipherBuffer, to, nil, nil, nil)
+	if err != nil {
+		return encryptedMessage, err
+	}
+	writeCloser.Write(message)
+	writeCloser.Close()
+	encryptedMessage = string(cipherBuffer.Bytes())
 	return
 }
