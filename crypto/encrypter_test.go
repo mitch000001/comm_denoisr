d6ea4ee1ae800d3f6423538ab3427060c51c5fee
Michael Wagner <mitch.wagna@gmail.com>
2014-05-11 18:39:18 +0200

    Add support for hidden encryption

--- crypto/encrypter_test.go
+++ crypto/encrypter_test.go
@@ -3,6 +3,7 @@ package crypto
 import (
 	"bytes"
 	"code.google.com/p/go.crypto/openpgp"
+	"code.google.com/p/go.crypto/openpgp/armor"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -10,13 +11,15 @@ import (
 )
 
 var encrypter Encrypter
+var privring openpgp.EntityList
+var promptFunction openpgp.PromptFunction
 
 func init() {
 	privringFile, err := os.Open("../test_keyring.gpg")
 	if err != nil {
 		panic(err)
 	}
-	privring, err := openpgp.ReadKeyRing(privringFile)
+	privring, err = openpgp.ReadKeyRing(privringFile)
 	if err != nil {
 		privring, err = openpgp.ReadArmoredKeyRing(privringFile)
 		if err != nil {
@@ -24,12 +27,13 @@ func init() {
 		}
 	}
 	encrypter = NewOpenPgPEncrypter(privring)
-	decrypter = NewOpenPgPDecrypter(privring, openpgp.PromptFunction(func(keys []openpgp.Key, symmetric bool) (password []byte, err error) {
+	promptFunction = openpgp.PromptFunction(func(keys []openpgp.Key, symmetric bool) (password []byte, err error) {
 		if len(keys) > 0 {
 			keys[0].PrivateKey.Decrypt([]byte("test1234"))
 		}
 		return []byte("test1234"), nil
-	}))
+	})
+	decrypter = NewOpenPgPDecrypter(privring, promptFunction)
 }
 
 func TestEncrypt(t *testing.T) {
@@ -90,6 +94,57 @@ func TestEncryptFor(t *testing.T) {
 
 	decryptedMessage := string(decryptedBytes)
 	if decryptedMessage != expectedMessage {
-		t.Fatalf("expected %v to equal %v", decryptedMessage, expectedMessage)
+		t.Fatalf("expected %v to equal %v\n", decryptedMessage, expectedMessage)
 	}
+}
+
+func TestEncryptForHidden(t *testing.T) {
+	file, err := os.Open("../decrypted_message.txt")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	plainBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+	expectedMessage := string(plainBytes)
+	encryptedMessage, err := encrypter.EncryptForHidden(bytes.NewReader(plainBytes), []string{"test@example.com"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pgpBlock, err := armor.Decode(strings.NewReader(encryptedMessage))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	md, err := openpgp.ReadMessage(pgpBlock.Body, privring, promptFunction, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	encryptedToKeyId := md.EncryptedToKeyIds[0]
+	if encryptedToKeyId != uint64(0) {
+		t.Fatalf("expected encryptedToKeyId to equal 0, got %X\n", encryptedToKeyId)
+	}
+
+	for _, entity := range privring {
+		for _, s := range entity.Subkeys {
+			if s.PublicKey.KeyId == uint64(0) {
+				t.Fatalf("expected KeyId from subkey with fingerprint %v not to equal 0\n", s.PublicKey.Fingerprint)
+			}
+		}
+	}
+
+	decryptedBytes, err := ioutil.ReadAll(md.LiteralData.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	decryptedMessage := string(decryptedBytes)
+	if decryptedMessage != expectedMessage {
+		t.Fatalf("expected %v to equal %v\n", decryptedMessage, expectedMessage)
+	}
+
 }
