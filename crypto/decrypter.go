47bae62290c018f7a8c6a1f024e0a0fac249baec
Michael Wagner <mitch.wagna@gmail.com>
2014-05-16 11:14:13 +0200

    Introduce NoOpStructs to formalize the interface with dummy implementation

--- crypto/decrypter.go
+++ crypto/decrypter.go
@@ -9,9 +9,13 @@ import (
 	"io"
 )
 
+// Plain represents the decrypted data
 type Plain interface {
+	// Returns the raw body of the decrypted content
 	Body() io.Reader
+	// Returns whether the decrypted content is binary or not
 	IsBinary() bool
+	// Returns the filename of the decrypted content
 	FileName() string
 }
 
@@ -19,6 +23,32 @@ type Decrypter interface {
 	Decrypt(io.Reader) (Plain, error)
 }
 
+type NoOpPlain struct {
+	body     io.Reader
+	isBinary bool
+	fileName string
+}
+
+func (this *NoOpPlain) Body() io.Reader {
+	return this.body
+}
+
+func (this *NoOpPlain) IsBinary() bool {
+	return this.isBinary
+}
+
+func (this *NoOpPlain) FileName() string {
+	return this.fileName
+}
+
+type NoOpDecrypter struct{}
+
+func (this *NoOpDecrypter) Decrypt(message io.Reader) (Plain, error) {
+	return &NoOpPlain{body: message, isBinary: false, fileName: ""}, nil
+}
+
+// This wraps packet.LiteralData from the openpgp packet
+// to conform the defined Plain interface
 type OpenPgpPlain struct {
 	ld packet.LiteralData
 }
--- crypto/encrypter.go
+++ crypto/encrypter.go
@@ -10,6 +10,8 @@ import (
 )
 
 type Encrypter interface {
+	// Encrypt the given message with the provided password
+	// Returns the encrypted message or an error
 	Encrypt(message io.Reader, password string) (string, error)
 	// Encrypt the given message for all recipients in to
 	// Returns the encrypted message or an error
@@ -17,6 +19,24 @@ type Encrypter interface {
 	// Encrypt the given message for all recipients in to and hides the recipients in the encrypted message
 	// Returns the encrypted message or an error
 	EncryptForHidden(message io.Reader, to []string) (string, error)
+}
+
+type NoOpEncrypter struct{}
+
+func (this *NoOpEncrypter) Encrypt(message io.Reader, password string) (string, error) {
+	messageBytes, err := ioutil.ReadAll(message)
+	if err != nil {
+		return "", err
+	}
+	return string(messageBytes), nil
+}
+
+func (this *NoOpEncrypter) EncryptFor(message io.Reader, to []string) (string, error) {
+	return this.Encrypt(message, "")
+}
+
+func (this *NoOpEncrypter) EncryptForHidden(message io.Reader, to []string) (string, error) {
+	return this.Encrypt(message, "")
 }
 
 type OpenPgPEncrypter struct {
